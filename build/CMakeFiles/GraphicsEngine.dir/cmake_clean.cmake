e0ac9d8c109608a46f47f5a91ab37bb79d61a991
RyanMacDev <rmacdb425@gmail.com>
2025-01-05 20:14:52 -0600

    Start of Graphx

---
+++ build/CMakeFiles/GraphicsEngine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GraphicsEngine.dir/src/main.cpp.o"
+  "CMakeFiles/GraphicsEngine.dir/src/main.cpp.o.d"
+  "GraphicsEngine"
+  "GraphicsEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GraphicsEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
